208474ae41b7ea6b6e33dd8bcb627f171498de9a
Joshua Peoples <46235676+jpcoder12@users.noreply.github.com>
2022-09-14 12:29:25 +0700

    Add data for results
    Added data variable to connect to render into the results page.

--- controllers/results.js
+++ controllers/results.js
@@ -2,6 +2,57 @@
 const express = require('express')
 const router = express.Router()
 
+let data = [
+    {
+        title: 'Final Fantasy VII REMAKE',
+        description: 'A very good action JRPG made by Square Enix',
+        url: 'https://www.playstation.com/th-th/games/final-fantasy-vii-remake/',
+        links: 
+            [
+                {
+                    title: 'The original FFVII',
+                    url: 'https://en.wikipedia.org/wiki/Final_Fantasy_VII'
+                }, 
+                {
+                    title: 'Hollow Skies - Beautiful',
+                    url: 'https://www.youtube.com/watch?v=ujwkwnpqwpE'
+                }
+            ]
+    },
+    {
+        title: 'Kingdom Hearts',
+        description: 'My favorite video game series EVER!',
+        url: 'https://www.kingdomhearts.com',
+        links: 
+            [
+                {
+                    title: 'Kingdom Hearts 2',
+                    url: 'https://www.kingdomhearts.com'
+                }, 
+                {
+                    title: 'Epic Boss Battle',
+                    url: 'https://www.youtube.com/watch?v=_B-pk3C7ypk'
+                }
+            ]
+    },
+    {
+        title: 'LittleBigPlanet',
+        description: 'The game that influenced my interest in software development',
+        url: 'https://en.wikipedia.org/wiki/LittleBigPlanet#LittleBigPlanet_2',
+        links: 
+            [
+                {
+                    title: 'Velvet--Audio music',
+                    url: 'https://www.youtube.com/watch?v=9KCg_8MzpCo&list=PLAF81BC00A411FFF7'
+                }, 
+                {
+                    title: 'Dreams',
+                    url: 'https://indreams.me'
+                }
+            ]
+    }
+]
+
 // Create POST controller
 router.get('/', (req, res) => {
     res.render('results')
