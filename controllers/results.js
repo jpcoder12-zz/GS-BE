a8674f8f167e25cdcb0aa6e965256d0bcf52e50c
Joshua Peoples <46235676+jpcoder12@users.noreply.github.com>
2022-09-15 14:56:51 +0700

    Result vs Results
    If there is 0 or multiple result(s) vs if there is only one result.
    
    This update accounts for that on the results page. Also updated some variables to accommodate this change.

--- controllers/results.js
+++ controllers/results.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
-let data = [
+let rawData = [
     {
         title: 'Final Fantasy VII REMAKE',
         description: 'A very good action JRPG made by Square Enix',
@@ -68,16 +68,29 @@ let data = [
             ]
     }
 ]
-const getDatafromSearch = (srchInput, data) => {
+const getDatafromSearch = (srchInput, rawData) => {
 // for each element in the data, split the words in the title (to turn into arr) and search if it includes the search input
-  let results = (srchInput == '*') ? data : data.filter(elem => elem.title.toLowerCase().includes(srchInput.toLowerCase()))
+  let results = (srchInput == '*') ? rawData : rawData.filter(elem => elem.title.toLowerCase().includes(srchInput.toLowerCase()))
   return results
 }
 
+// Returns 'result' or plural 'results' depending on how many results there are
+const countResults = arr => {
+    if (arr.length == 0 || arr.length >= 2){
+        return `${arr.length} Results`
+    }
+    else {
+        return `${arr.length} Result`
+    }
+}
 // Create POST controller
 router.post('/', (req, res) => {
-    res.render('results', {data: getDatafromSearch(req.body.search, data)})
+    res.render('results', {data: getDatafromSearch(req.body.search, rawData), 
+        resultCount: countResults(getDatafromSearch(req.body.search, rawData))})
 })
 
+router.get('/', (req, res) => {
+    res.render('results')
+})
 // Export module
 module.exports = router
