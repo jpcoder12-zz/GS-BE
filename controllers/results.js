2393b837183eb95803adb9a4a8571a997fc94862
Joshua Peoples <46235676+jpcoder12@users.noreply.github.com>
2022-09-16 15:21:04 +0700

    Regex addition
    Allows search results that are fuzzy/partial match.

--- controllers/results.js
+++ controllers/results.js
@@ -21,7 +21,7 @@ const Results = require('../models/results')
 // Create POST controller
 router.post('/', async (req, res) => {
     let data = await Results.find({
-        title: req.body.search
+        title: {$regex: req.body.search, $options: 'i'}
     })
     console.log(`Data: ${data}`)
 
